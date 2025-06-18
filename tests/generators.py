4ed91a1a886e20c31fe938482e79ed7de4ad6fa2
fzyzcjy <ch271828n@outlook.com>
2025-06-18 15:49:55 +0800

    more

--- tests/generators.py
+++ tests/generators.py
@@ -30,7 +30,8 @@ def enumerate_grouped_contiguous():
             *[
                 x
                 for simulated_num_ranks in [4, 8, 16, 24, 32]
-                for num_tokens in [4096, 8192, 16384, 32768, 65536]
+                # TODO more tokens
+                for num_tokens in [2048, 4096, 8192, 16384]
                 for x in create_ones(simulated_num_ranks=simulated_num_ranks, num_tokens=num_tokens)
             ],
     ):
