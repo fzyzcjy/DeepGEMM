e228e13aec5658b93dfd1b333dd304b5da0e8dbf
fzyzcjy <ch271828n@outlook.com>
2025-06-18 15:53:30 +0800

    more

--- tests/generators.py
+++ tests/generators.py
@@ -36,6 +36,7 @@ def enumerate_grouped_contiguous():
                 for simulated_num_ranks in [4, 8, 16, 24, 32]
                 # TODO more tokens
                 for num_tokens in [2048, 4096, 8192, 16384]
+                for _repeat in range(10)
             ],
     ):
         # NOTE MODIFIED
