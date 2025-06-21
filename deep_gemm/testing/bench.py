adef57334aadc65a059ee9d193e5d76cfd3eb3f9
fzyzcjy <ch271828n@outlook.com>
2025-06-21 22:09:00 +0800

    mroe

--- deep_gemm/testing/bench.py
+++ deep_gemm/testing/bench.py
@@ -111,7 +111,6 @@ def bench_kineto(fn, kernel_names, num_tests: int = 30,
     assert isinstance(kernel_names, str) or isinstance(kernel_names, tuple)
     is_tuple = isinstance(kernel_names, tuple)
     prof_lines = profiler.key_averages().table(sort_by='cuda_time_total', max_name_column_width=100).split('\n')
-    print(f"{prof_lines=}")
     kernel_names = (kernel_names, ) if isinstance(kernel_names, str) else kernel_names
     assert all([isinstance(name, str) for name in kernel_names])
     if not with_multiple_kernels:
