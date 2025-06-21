d3d2c8a0b5282c2bdb1bbd0de1dc869fbe993c8d
fzyzcjy <ch271828n@outlook.com>
2025-06-21 22:15:40 +0800

    more

--- tests/test_core.py
+++ tests/test_core.py
@@ -115,9 +115,9 @@ def test_m_grouped_gemm_masked() -> None:
 
         # noinspection PyShadowingNames
         def test_func():
-            print("hi test_func start")
+            print(f"hi test_func START {d.sum()=}")
             deep_gemm.fp8_m_grouped_gemm_nt_masked(a, b, d, masked_m, m)
-            print("hi test_func end")
+            print(f"hi test_func END {d.sum()=}")
 
         # Test performance with fixed shapes
         t = bench_kineto(test_func, 'fp8_gemm',
